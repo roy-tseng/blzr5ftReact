16b3d0c8f0bbb6f32b47156bd72b63286fcf8cb9
joey_tseng <joey_tseng@wistron.com>
2020-11-21 02:51:26 +0800

    modify react

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './wwwroot/js'),
     filename: "[name]",
-    libraryTarget: 'umd', // 采用通用模块定义
-    //libraryExport: 'default', // 兼容 ES6(ES2015) 的模块系统、CommonJS 和 AMD 模块规范
-    //globalObject: 'this', // 兼容node和浏览器运行，避免window is not undefined情况
+    libraryTarget: 'umd', 
+    //libraryExport: 'default', 
+    //globalObject: 'this',
     library: "InfoLinkComponents"
   },
   module: {
